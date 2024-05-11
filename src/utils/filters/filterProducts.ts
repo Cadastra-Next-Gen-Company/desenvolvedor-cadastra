c2ad9f1fcd761600daceba9fef463679f1210bd5
Pablo Alves <ribeirojr1522@gmail.com>
2024-05-11 16:13:15 -0300

    feat: :sparkles: filtered per price product

--- src/utils/filters/filterProducts.ts
+++ src/utils/filters/filterProducts.ts
@@ -5,10 +5,13 @@ import { generateCardProducts } from "../product"
 export function filterProducts({ products }: { products: ListProduct }): ListProduct {
   const containerProducts = document.getElementById("products")
   const colorCheckboxes = document.querySelectorAll(".color-checkbox")
+  const priceCheckbox = document.querySelectorAll(".price-checkbox")
   const sizesButton = document.querySelectorAll(`.${classButtonSizeActive}`)
 
   const colors: Array<string> = []
   const sizes: Array<string> = []
+  let fromPrice: number | null;
+  let toPrice: number | null;
 
   colorCheckboxes.forEach((checkbox: HTMLInputElement) => {
     if (checkbox.checked) {
@@ -22,18 +25,56 @@ export function filterProducts({ products }: { products: ListProduct }): ListPro
     }
   })
 
-  const filterColor = products.filter(product => colors.includes(product.color))
-  const filterProductColors = filterColor.length > 0 ? filterColor : products
+  priceCheckbox.forEach((checkbox: HTMLInputElement) => {
+    if (checkbox.checked) {
+      const value = checkbox.value
+      const regex = /R\$\s*(\d+)\s*até\s*R\$\s*(\d+)/;
+      const match = value.match(regex);
 
-  const filterSize = filterProductColors.filter(product => {
+      if (match) {
+        const [_, fromPriceString, toPriceString] = match
+
+        if (fromPriceString && toPriceString) {
+          fromPrice = parseInt(fromPriceString)
+          toPrice = parseInt(toPriceString)
+        }
+      } else {
+        if (value === "apartir de R$ 500") {
+          fromPrice = 500
+          toPrice = Infinity
+        } else {
+          fromPrice = null
+          toPrice = null
+        }
+      }
+    }
+  })
+
+  const filterColor = products.filter(product => {
+    if (colors.length > 0) {
+      return colors.includes(product.color)
+    } else {
+      return true
+    }
+  })
+
+  const filterSize = filterColor.filter(product => {
     if (sizes.length > 0) {
       return product.size.some(size => sizes.includes(size))
     } else {
       return true
     }
-  }
-  )
+  })
+
+  const filterPrice = filterSize.filter(product => {
+    if (fromPrice > -1 && toPrice > fromPrice) {
+      return product.price >= fromPrice && product.price <= toPrice
+    } else {
+      return true
+    }
+  })
+
   containerProducts.innerHTML = ""
-  generateCardProducts({ products: filterSize })
+  generateCardProducts({ products: filterPrice })
   return filterSize
 }
