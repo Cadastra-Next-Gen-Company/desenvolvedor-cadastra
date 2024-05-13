f31a9800b65c315de276eaa6271ea6bed32d40f8
Pablo Alves <ribeirojr1522@gmail.com>
2024-05-13 15:53:08 -0300

    feat: :sparkles: handle filter mobile

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,23 +2,21 @@ import {
   addEventInCartButton,
   changeFilters,
   generateCardProducts,
-  generateFilterColors,
-  generateFilterPrice,
-  generateFilterSize,
   getProducts,
+  handleVisibleFilter,
   quantityProductInCart
 } from "../utils";
+import { generateFilters } from "../utils/filters/generateFilters";
 
 async function main() {
   const products = await getProducts()
-
-  generateFilterColors({ products })
-  generateFilterSize({ products })
-  generateFilterPrice()
+  
+  generateFilters({ products })
   generateCardProducts({ products })
   changeFilters({ products })
   quantityProductInCart()
   addEventInCartButton()
+  handleVisibleFilter()
 }
 
 document.addEventListener("DOMContentLoaded", main);
--- src/utils/filters/index.ts
+++ src/utils/filters/index.ts
@@ -1,9 +1,8 @@
 export { changeFilters } from "./changeFilters"
 export { filterProducts } from "./filterProducts"
-export { generateFiltersCheckbox } from "./generateFiltersCheckbox"
 export { generateFilterColors } from "./generateFilterColors"
 export { generateFilterPrice } from "./generateFilterPrice"
 export { generateFilterSize } from './generateFilterSize'
-export { generateFilterButtons } from "./generateFilterButtons"
 export { generateProductNotFound } from "./generateProductNotFound"
 export { clearFilters } from "./clearFilters"
+export { handleVisibleFilter } from "./handleVisibleFilter"
